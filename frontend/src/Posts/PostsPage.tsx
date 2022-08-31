179d994f7fca6481d5887737176644d9eff9a5fd
Nick Osman <Nick.Osman@softwire.com>
2022-08-31 14:05:17 +0100

    working posts page

--- frontend/src/Posts/PostsPage.tsx
+++ frontend/src/Posts/PostsPage.tsx
@@ -1,26 +1,58 @@
 import { useEffect, useState } from "react"
+import {PostUserModel} from "../../../src/models/api/postModel"
 
-interface PostUserModel {
+interface PostModel {
     id: number;
-    name: string;
-    username: string;
-    email: string;
+    message: string;
+    imageUrl: string;
+    createdAt: moment.Moment;
+    postedBy: PostUserModel;
+    likedBy: PostUserModel[];
+    dislikedBy: PostUserModel[];
 }
 
 export default function PostsPage() {
 
-    const [postList, setPostList] = useState<null | PostUserModel[]>(null)
+    const [postsData, setPostsData] = useState<{next: string | null; 
+                                                previous: string|null; 
+                                                results : PostModel[];
+                                                total: number}>()
+
 
     useEffect(() =>{
-        fetch("http://localhost:3001/posts/").then(response => response.json())
-                                             .then(posts => setPostList(posts))
-    }, []);
-    
-    
-    if (postList){
-        postList.forEach()
+        fetch("http://localhost:3001/posts").then(response => response.json())
+                                             .then(posts => setPostsData(posts))
+                                            }, []);
+
+
+                                     
+
+
+    if (postsData){
+
+        return(
+            <div>
+                {postsData.results.map(post => {
+                    return(
+                    <div className="post">
+                        <p>
+                            {post.message}
+                        </p>
+                        <img src={post.imageUrl} />
+                    </div>
+                    )
+                })}
+            </div>
+        )
     }
+    else{
+        return <></>
+    }
+
     
-    return <>hi
-    </>
+    
+
+
+
+    
 }
